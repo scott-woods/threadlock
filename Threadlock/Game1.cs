dc91e9f42f49201834196449d770a58f0d415bf4
scott-woods <scott_woods44@yahoo.com>
2024-01-24 18:09:23 -0500

    initial commit with fna and nez submodules

---
+++ Threadlock/Game1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Nez;
+
+namespace Threadlock
+{
+    public class Game1 : Core
+    {
+
+    }
+}
